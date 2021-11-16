79fba98153d120518c9226f0843786dd9b30a36b
IcelandicPenguin <Brandonryingling@gmail.com>
2021-11-15 22:43:45 -0500

    Assets Files

---
+++ Apple Picker/Assets/ApplePicker.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ApplePicker : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    public GameObject basketPrefab;
+    public int numBasket = 3;
+    public float basketBottomY = -14f;
+    public float basketSpacingY = 2f;
+    public List<GameObject> basketList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        basketList = new List<GameObject>();
+        for (int i=0; i<numBasket; i++)
+        {
+            GameObject tBasketGo = Instantiate<GameObject>(basketPrefab);
+            Vector3 pos = Vector3.zero;
+            pos.y = basketBottomY + (basketSpacingY * i);
+            tBasketGo.transform.position = pos;
+            basketList.Add(tBasketGo);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void AppleDestroyed()
+    {
+        //Destroy all of the falling apples
+        GameObject[] tAppleArray = GameObject.FindGameObjectsWithTag("Apple");
+        foreach (GameObject tGo in tAppleArray)
+        {
+            Destroy(tGo);
+        }
+        //Destroy one of the baskets
+        //Get the index of the last Basket in basketList
+        int basketIndex = basketList.Count - 1;
+
+        //Get a refrence to that Basket GameObject
+        GameObject tBasketGO = basketList[basketIndex];
+
+        //Remove the basket from the list and destroy the GameObject
+        basketList.RemoveAt(basketIndex);
+        Destroy(tBasketGO);
+
+        //If there are no baskets left, restart the game
+        if (basketList.Count == 0)
+        {
+            SceneManager.LoadScene("Scene_0");
+        }
+    }
+}
